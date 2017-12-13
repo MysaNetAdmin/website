07093e1661b3cf0e2d35ca57c0b397c7fbc0ab6b
Samy Hussaein <samy.hussaein@epita.fr>
2017-12-13 13:58:58 +0100

    FOUTU CONTACT

--- gesture-online/contact.php
+++ gesture-online/contact.php
@@ -1,17 +1,19 @@
 <?php
-$to = "contact@samyhussaein.com"; // this is your Email address
-$from = $_POST['email']; // this is the sender's Email address
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$subject = $_POST['subject'];
-$message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+if(isset($_POST['submit'])){
+    $to = "contact@samyhussaein.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = $_POST['subject'];
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-$headers = "From:" . $from;
-$headers2 = "From:" . $to;
-$mail = $smtp->send($to,$subject,$message,$headers);
-$mail = $smtp->send($from,$subject,$message2,$headers2); // sends a copy of the message to the sender
-echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    }
 ?>
 
 <!doctype html>

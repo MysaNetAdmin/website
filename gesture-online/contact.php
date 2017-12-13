8efa99d3fdbab001a63ab1f4b75046f16feda5f8
Samy Hussaein <samy.hussaein@epita.fr>
2017-12-13 14:33:38 +0100

    changed to .php again

--- gesture-online/contact.php
+++ gesture-online/contact.php
@@ -173,3 +173,20 @@
         <script src="js/jQuery-latest.js" type="text/javascript"></script>
     </body><!-- end body -->
 </html><!-- end html -->
+<?php
+if(isset($POST['submit'])){
+    $to = "contact@samyhussaein.com"; // this is your Email address
+    $from = 'email'; // this is the sender's Email address
+    $first_name = 'first_name';
+    $last_name = 'last_name';
+    $subject = 'subject';
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message);
+    mail($from,$subject,$message2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    }
+?>

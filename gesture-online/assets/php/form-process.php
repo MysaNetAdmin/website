8739a9e753124b3fa6531dd3edc27c562f7f27e5
Samy Hussaein <samy.hussaein@epita.fr>
2017-12-13 14:39:47 +0100

    changed to .html again and form-process

--- gesture-online/assets/php/form-process.php
+++ gesture-online/assets/php/form-process.php
@@ -1,12 +1,12 @@
 <?php
-if(isset($POST['submit'])){
+if(isset($_POST['submit'])){
     $to = "contact@samyhussaein.com"; // this is your Email address
-    $from = $POST['email']; // this is the sender's Email address
-    $first_name = $POST['first_name'];
-    $last_name = $POST['last_name'];
-    $subject = $POST['subject'];
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $POST['message'];
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = $_POST['subject'];
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;

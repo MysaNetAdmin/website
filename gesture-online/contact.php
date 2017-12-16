bde5852f1aa73de9233e6bef76c9ca4bc8143d36
Samy Hussaein <samy.hussaein@epita.fr>
2017-12-16 21:43:27 +0100

    all php in contact

--- gesture-online/contact.php
+++ gesture-online/contact.php
@@ -173,4 +173,21 @@
         <script src="js/jQuery-latest.js" type="text/javascript"></script>
     </body><!-- end body -->
 </html><!-- end html -->
-<?php include_once "assets/php/form-process.php"; ?>
+<?php
+mail("samy.hussaien@gmail.com", "test", "KAPPA TEST")
+if(isset($_POST['submit'])){
+    $to = "contact@samyhussaein.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = $_POST['subject'];
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message);
+    mail($from,$subject,$message2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    }
+?>
